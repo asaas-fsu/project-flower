c7335c40f9574e5783f2dde911f361d0828b3749
DESKTOP-76MN01T\Daniel <ddalegib@comcast.net>
2022-03-19 12:34:39 -0400

    Added login and create account functionality.

--- Node-Server/database connector/app.js
+++ Node-Server/database connector/app.js
@@ -4,12 +4,13 @@ const port = 3000
 const path = require('path');
 var mysql = require ('mysql');
 var bodyParser = require('body-parser');
+const { redirect } = require('express/lib/response');
 
 
-var connection = mysql.createConnection({
-    host: 'student-space.c9udzjhcbdqh.us-east-1.rds.amazonaws.com', 
-    user: 'root',
-    password: 'password',
+var connection = mysql.createPool({
+    host: 'financial-lit.clk0inmtrme1.us-east-1.rds.amazonaws.com',
+    user: 'developer',
+    password: 'developer1',
     database: 'student_space'
 });
 
@@ -22,30 +23,70 @@ app.get('/', (req, res) => {
 
 app.get('/students', async (req, res) => {
     connection.query("SELECT * FROM user", function(err, rows) {
-       if (err) throw err;
+       if (err) throw err
        res.send(rows); 
     });
          
 })
 
-app.get('/home', (req, res) => {
-    app.use(express.static('public'))
-    res.sendFile('index.html', {root: './public/html pages'});
-})
-
-app.post('/login', function(req, res, next) {
+app.post('/createAccount', function(req, res, next) {
     var user = req.body.username;
     var pass = req.body.password;
     var conf_pass = req.body.confirm_pass;
     var email = req.body.email;
-    
-    var sql = `INSERT INTO user (username, password, first_name, last_name, email) VALUES ("${user}", "${pass}", "${'First_Name'}","${'Last_Name'}", "${email}")`;
-    connection.query(sql, function(err, result) {
-      if (err) throw err;
-      console.log('record inserted');
-      res.redirect('/');
-    });
+    var firstName = req.body.first_name;
+    var lastName = req.body.last_name;
+      
+    connection.query(`SELECT username FROM user WHERE username = "${user}"`, function (err, result, fields) {
+        if (err) throw err;
+        if(JSON.stringify(result).length > 2){
+          //username exists do not allow user to create account.
+          console.log("USERNAME EXISTS: Do not allow account creation.")
+          res.redirect('/')
+        }
+        else{
+          var sql = `INSERT INTO user (username, password, first_name, last_name, email) VALUES ("${user}", "${pass}", "${firstName}","${lastName}", "${email}")`;
+          connection.query(sql, function(err, result) {
+            if (err) throw err;
+            console.log('record inserted');
+            res.redirect('/');
+          });
+        }
+      }); 
+
+
+
   });
+
+  app.post('/login', function(req, res, next) {
+    var user = req.body.log_username;
+    var pass = req.body.log_pass;
+      
+    connection.query(`SELECT username FROM user WHERE username = "${user}"`, function (err, result, fields) {
+        if (err) throw err;
+        if(JSON.stringify(result).length > 2){
+          connection.query(`SELECT password FROM user WHERE username = "${user}" AND password = "${pass}"`, function (err, result, fields) {
+            if (err) throw err;
+            if(JSON.stringify(result).length > 2){
+              //username exists continue to main page.
+              console.log("USERNAME EXISTS: Continue to main page.")
+              res.redirect('/')
+            }
+            else{
+              console.log("Invlaid username/password Combination.")
+              res.redirect('/')
+            }
+          });
+        }
+        else{
+          console.log("Invlaid username/password Combination.")
+          res.redirect('/')
+        }
+      }); 
+
+
+
+  });  
 
 app.listen(port, () => {
     console.log(`app listning on port ${port}`)
