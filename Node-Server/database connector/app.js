afe716697b241dad7415b956f2b8e22a555dcae4
David Davis <dman774100@gmail.com>
2022-04-04 19:37:29 -0400

    Linked pages. Still need file names for discussion

--- Node-Server/database connector/app.js
+++ Node-Server/database connector/app.js
@@ -4,12 +4,15 @@ const port = 3000
 const path = require('path');
 var mysql = require ('mysql');
 var bodyParser = require('body-parser');
+// need cookieParser middleware before we can do anything with cookies
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
 
 
 var connection = mysql.createConnection({
-    host: 'student-space.c9udzjhcbdqh.us-east-1.rds.amazonaws.com', 
-    user: 'root',
-    password: 'password',
+    host: 'financial-lit.clk0inmtrme1.us-east-1.rds.amazonaws.com', 
+    user: 'developer',
+    password: 'developer1',
     database: 'student_space'
 });
 
@@ -17,7 +20,16 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
     app.use(express.static('public'))
-    res.sendFile('instructorLogin.html', {root: './public/html pages'});
+    var cookies = req.cookies;
+    if (cookies.user === undefined) {
+        res.sendFile('instructorLogin.html', {root: './public/html pages'});
+    }
+    else if (cookies.instructor === "false") {
+        res.sendFile('mainsite-student.html', {root: './public/html pages'});
+    }
+    else if (cookies.instructor === "true") {
+        res.sendFile('mainsite-instructor.html', {root: './public/html pages'});
+    }
 })
 
 app.get('/students', async (req, res) => {
@@ -28,25 +40,133 @@ app.get('/students', async (req, res) => {
          
 })
 
-app.get('/home', (req, res) => {
+app.post('/login', function(req, res, next) {
+    var user = req.body.log_username;
+    var pass = req.body.log_pass;
+      
+    connection.query(`SELECT * FROM user WHERE username = "${user}"`, function (err, result, fields) {
+        if (err) throw err;
+        if(result[0] !== undefined){
+            if(result[0].password === pass) {
+                //username matches password continue to main page.
+                // check if client sent cookie
+                var cookies = req.cookies;
+                if (cookies.user === undefined) {
+                    res.cookie('user',user, { maxAge: 900000});
+                    if (result[0].status === 1) {
+                        res.cookie('instructor',"true", { maxAge: 900000});
+                    }
+                    else {
+                        res.cookie('instructor', "false", {maxAge: 900000});
+                    }
+                    console.log('cookie created successfully');
+                } else {
+                    // yes, cookie was already present 
+                    console.log('cookie exists', cookies);
+                }
+                console.log("USERNAME EXISTS: Continue to main page.")
+                res.redirect('/')
+            }
+            else {
+                console.log("Invlaid username/password Combination.")
+                res.redirect('/')
+            }
+        }
+        else{
+            console.log("Invlaid username/password Combination.")
+            res.redirect('/')
+        }
+    }); 
+});
+
+app.get('/lms', (req, res) => {
     app.use(express.static('public'))
-    res.sendFile('index.html', {root: './public/html pages'});
+    var cookies = req.cookies;
+    if (cookies.user === undefined) {
+        res.sendFile('instructorLogin.html', {root: './public/html pages'});
+    }
+    else if (cookies.instructor === "false") {
+        res.sendFile('lms-student.html', {root: './public/html pages'});
+    }
+    else if (cookies.instructor === "true") {
+        res.sendFile('lms-instructor.html', {root: './public/html pages'});
+    }
+    else {
+        res.send(`<script>alert("Error: User logged in but not designated instructor or student."); window.location.href = "/"; </script>`);
+    }
 })
 
-app.post('/login', function(req, res, next) {
-    var user = req.body.username;
-    var pass = req.body.password;
-    var conf_pass = req.body.confirm_pass;
-    var email = req.body.email;
-    
-    var sql = `INSERT INTO user (username, password, first_name, last_name, email) VALUES ("${user}", "${pass}", "${'First_Name'}","${'Last_Name'}", "${email}")`;
-    connection.query(sql, function(err, result) {
-      if (err) throw err;
-      console.log('record inserted');
-      res.redirect('/');
-    });
-  });
+app.get('/lms/poll', (req, res) => {
+    app.use(express.static('public'))
+    var cookies = req.cookies;
+    if (cookies.user === undefined) {
+        res.sendFile('instructorLogin.html', {root: './public/html pages'});
+    }
+    else if (cookies.instructor === "false") {
+        res.sendFile('polls-student.html', {root: './public/html pages'});
+    }
+    else if (cookies.instructor === "true") {
+        res.sendFile('polls-instructor.html', {root: './public/html pages'});
+    }
+    else {
+        res.send(`<script>alert("Error: User logged in but not designated instructor or student."); window.location.href = "/"; </script>`);
+    }
+})
+
+app.get('/lms/content', (req, res) => {
+    app.use(express.static('public'))
+    var cookies = req.cookies;
+    if (cookies.user === undefined) {
+        res.sendFile('instructorLogin.html', {root: './public/html pages'});
+    }
+    else if (cookies.instructor === "false") {
+        res.sendFile('assignment-download.html', {root: './public/html pages'});
+    }
+    else if (cookies.instructor === "true") {
+        res.sendFile('assignment-upload.html', {root: './public/html pages'});
+    }
+    else {
+        res.send(`<script>alert("Error: User logged in but not designated instructor or student."); window.location.href = "/"; </script>`);
+    }
+})
+
+app.get('/lms/discussion', (req, res) => {
+    app.use(express.static('public'))
+    var cookies = req.cookies;
+    if (cookies.user === undefined) {
+        res.sendFile('instructorLogin.html', {root: './public/html pages'});
+    }
+    else if (cookies.instructor === "false") {
+        res.sendFile();
+    }
+    else if (cookies.instructor === "true") {
+        res.sendFile();
+    }
+    else {
+        res.send(`<script>alert("Error: User logged in but not designated instructor or student."); window.location.href = "/"; </script>`);
+    }
+})
+
+app.get('/lms/groups', (req, res) => {
+    app.use(express.static('public'))
+    var cookies = req.cookies;
+    if (cookies.user === undefined) {
+        res.sendFile('instructorLogin.html', {root: './public/html pages'});
+    }
+    else if (cookies.instructor === "false") {
+        res.sendFile('groups-students.html', {root: './public/html pages'});
+    }
+    else if (cookies.instructor === "true") {
+        res.sendFile('create-groups-instructor.html', {root: './public/html pages'});
+    }
+    else {
+        res.send(`<script>alert("Error: User logged in but not designated instructor or student."); window.location.href = "/"; </script>`);
+    }
+})
 
 app.listen(port, () => {
     console.log(`app listning on port ${port}`)
 })
+
+// let static middleware do its job
+app.use(express.static('public'));
---
+++ Node-Server/database connector/public/javascripts/cookie.js
@@ -0,0 +1,20 @@
+let user = getCookie("user");
+if (user !== "") {
+    document.getElementById("userButton").innerHTML = user + "<br>" + "Profile";
+}
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
