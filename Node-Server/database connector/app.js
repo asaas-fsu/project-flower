526f7ca3ff88c8900df7c07779d6b68def082bca
Abadacus <mridgely@students.fairmontstate.edu>
2022-04-14 21:02:02 -0400

    Displaying divs for groups
    Also removed unnecessary app.js parts that are not needed for this part of the project

--- Node-Server/database connector/app.js
+++ Node-Server/database connector/app.js
@@ -35,40 +35,7 @@ app.get('/students', async (req, res) => {
          
 })
 
-app.get('/assignment-upload', (req, res) => {
-    app.use(express.static('public'))
-    res.sendFile('assignment-upload.html', {root: './public/html pages'});
-})
-
-app.get('/create-groups', (req, res) => {
-    app.use(express.static('public'))
-    res.sendFile('create-groups-instructor.html', {root: './public/html pages'});
-})
-
-app.post('/createAccount', function(req, res, next) {
-    var user = req.body.username;
-    var pass = req.body.password;
-    var conf_pass = req.body.confirm_pass;
-    var email = req.body.email;
-    var firstName = req.body.first_name;
-    var lastName = req.body.last_name;
-    
-    if(queries.user_exists(user)){
-        //username exists do not allow user to create account.
-        console.log("USERNAME EXISTS: Do not allow account creation.")
-        res.send(`<script>alert("An account with that username already exists, please try again."); window.location.href = "/"; </script>`);
-    }
-     else{
-        var sql = `INSERT INTO user (username, password, first_name, last_name, email, status) VALUES ("${user}", "${pass}", "${firstName}","${lastName}", "${email}", "${false}")`;
-        database.query(sql, function(err, result) {
-        if (err) throw err;
-        console.log('record inserted');
-        res.redirect('/');
-        });
-    }
-  });
-
-  app.post('/login', function(req, res, next) {
+app.post('/login', function(req, res, next) {
     var user = req.body.log_username;
     var pass = req.body.log_pass;
       
@@ -93,22 +60,10 @@ app.post('/createAccount', function(req, res, next) {
       }); 
   }); 
   
-  app.post('/fileUpload',(req, res) => {
-    let file;
-    var fileName = req.body.fileName;
-
-    if(!req.files || Object.keys(req.files).length === 0){
-        return res.status(400).send("File failed to upload.")
-    }
-
-    file = req.files.sampleFile;
-    var sql = `INSERT INTO assignment (assign_id, document) VALUES ("${fileName}", "${file}")`;
-    database.query(sql, function(err, result) {
-        if (err) throw err;
-        console.log('record inserted');
-        });
-    res.send("File uploaded.")    
-  })
+app.get('/lms/create-groups', (req, res) => {
+    app.use(express.static('public'))
+    res.sendFile('create-groups-instructor.html', {root: './public/html pages'});
+})
 
 app.listen(port, () => {
     console.log(`app listening on port ${port}`)
--- Node-Server/database connector/public/javascripts/create-groups-instructor.js
+++ Node-Server/database connector/public/javascripts/create-groups-instructor.js
@@ -1,8 +1,21 @@
-function getData(){
-      var data = [];
-      data[0] = document.getElementsByName('input')[0].value;
-      console.log(data); // Check the console for the values for every click.
-  return data;   
-  }
+function getData() {
+    var data = [];
+    var arrayDiv = [];
+    var numGroups = 0;
+    
+    data[0] = document.getElementsByName('input')[0].value;
+
+    var numGroups = data[0];
+    console.log(numGroups);
+
+    for (var i = 0; i < numGroups; i++) {
+        arrayDiv[i] = document.createElement('div');
+        arrayDiv[i].id = 'block' + i;
+        arrayDiv[i].style.backgroundColor = 'white';
+        arrayDiv[i].className = 'block' + i;
+        arrayDiv[i].textContent = i + 1;
+        document.body.appendChild(arrayDiv[i]);
+    }
+}
   
   var x = getData(); // When this is executed there will be no values in the input.
