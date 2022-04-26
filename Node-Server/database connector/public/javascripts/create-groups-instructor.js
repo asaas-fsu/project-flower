56327bd4e4b8e2b17e8898aa8a1c31fd20633005
Abadacus <mridgely@students.fairmontstate.edu>
2022-04-26 13:28:16 -0400

    Update create-groups-instructor.js

--- Node-Server/database connector/public/javascripts/create-groups-instructor.js
+++ Node-Server/database connector/public/javascripts/create-groups-instructor.js
@@ -22,7 +22,7 @@ function setupPage() {
     })
     .catch(function(error) {
         console.log(error);
-        alert("Error occured when fetching groups: Error message: " + error);
+        alert(error);
     });
 }
 
@@ -45,10 +45,10 @@ function viewGroup(id) {
             backButton.textContent = "Back";
             backButton.setAttribute("onClick", "javascript: setupPage();");
             groupList.appendChild(backButton);
-        })
+        })s
         .catch(function(error) {
             console.log(error);
-            alert("Error loading group. Error message: " + error);
+            alert(error);
         });
     }
     catch (exception) {
@@ -57,42 +57,16 @@ function viewGroup(id) {
     }
 }
 
-// function createGroup() {
-//     try {
-//         if (document.getElementById("input").value === "") {
-//             throw "Please enter a number!";
-//         }
-//         fetch("/createGroup", {
-//             method: 'POST',
-//             headers: {
-//                 'Content-Type': 'application/json'
-//             },
-//             body: JSON.stringify(data)
-//         })
-//         .then(function(response) {
-//             console.log('got data');
-//         })
-//         .catch(function(error) {
-//             console.log(error);
-//             throw "Error";
-//         });
-
-//         setupPage();
-//     }
-//     catch(exception) {
-//         alert(exception);
-//     }
-// }
 // This retrieves the number from the input field and creates sections.
 function getData() {
     var data = [];
-    var arrayDiv = [];
+    // var arrayDiv = [];
     var numGroups = 0;
     
     data[0] = document.getElementsByName('input')[0].value;
 
     numGroups = data[0];
-    console.log(numGroups);
+    console.log(data[0]);
 
     // for (var i = 0; i < numGroups; i++) {
     //     arrayDiv[i] = document.createElement('div');
@@ -104,4 +78,4 @@ function getData() {
     // }
 }
   
-  var x = getData(); // When this is executed there will be no values in the input.
+//   var x = getData(); // When this is executed there will be no values in the input.
